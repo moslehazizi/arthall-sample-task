ab7089270b5799daa5af0eedda93dd7f6a2996e2
mosleh <moslehazizi@gmail.com>
2024-02-26 18:22:42 +0330

    create activity model

---
+++ activities/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from accounts.models import CustomUser
+
+CHOICES = (
+    (1, 'poor'),
+    (2, 'below average'),
+    (3, 'average'),
+    (4, 'above average'),
+    (5, 'good'),
+    (6, 'great'),
+)
+
+class ActivityType(models.Model):
+    name = models.CharField(max_length=75)
+    need_to_aprove = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class ActivityStatus(models.Model):
+    status = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.status
+    
+class Activity(models.Model):
+    owner = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='activity_owner',
+    )
+    activity_title = models.ForeignKey(
+        ActivityType,
+        on_delete=models.CASCADE,
+        related_name='activity_type',
+    )
+    value = models.IntegerField(choices=CHOICES)
+    start_time =models.DateField()
+    end_time = models.DateField()
+    status = models.ForeignKey(
+        ActivityStatus,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='activity_status',
+    )
+    photos = models.ImageField(upload_to='photos',)
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.activity_title, self.owner
+
+
