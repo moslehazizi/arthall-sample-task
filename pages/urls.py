b9c93788267249f046ee2db23945ccb6e5b1b094
mosleh <moslehazizi@gmail.com>
2024-02-26 16:19:49 +0330

    add confirmation section for admins

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, RegisterConfirmationView, UserUpdateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('register-confirmation/', RegisterConfirmationView.as_view(), name='register_confirmation'),
+    path('user-update/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,29 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django.http.response import HttpResponseRedirect
+from django.views.generic import TemplateView, UpdateView, DetailView, ListView
+from accounts.models import CustomUser
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponseForbidden
+from django.shortcuts import render
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    
+    
+
+class RegisterConfirmationView(LoginRequiredMixin, ListView):
+    
+    model = CustomUser
+    context_object_name = 'not_confirmed_artists'
+    template_name = 'pages/register_confirmation.html'
+    fields = ('email', 'IsArtist', 'ConfirmUser',)
+    
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    template_name = 'pages/user_update.html'
+    fields = ('email', 'IsArtist', 'ConfirmUser',)
+
+    def get_success_url(self):
+        return self.request.GET.get('next')
+
